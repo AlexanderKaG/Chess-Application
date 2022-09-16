d18baaef482f21b491ea1ce033e562426df2c3a2
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-09-16 18:20:25 +0200

    first commit pushing pre-existing local repository

---
+++ src/test/java/se/alexander/portfolio/chess/ChessApplicationTests.java
@@ -0,0 +1,18 @@
+package se.alexander.portfolio.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+    
+    @Test
+    void firstTest() {
+
+    }
+
+}
