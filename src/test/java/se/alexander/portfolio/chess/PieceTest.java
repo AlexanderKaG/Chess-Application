2fab02d59793d8c0bb2266f87d1d5119541749ac
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-09-17 10:30:40 +0200

    Wrote test to check that a knight is a knight.

--- src/test/java/se/alexander/portfolio/chess/PieceTest.java
+++ src/test/java/se/alexander/portfolio/chess/PieceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static se.alexander.portfolio.chess.PieceType.KNIGHT;
 import static se.alexander.portfolio.chess.PieceType.PAWN;
 
 public class PieceTest {
@@ -14,8 +15,16 @@ public class PieceTest {
     @Test
     @DisplayName("A pawn is a pawn")
     void pawnIsPawn() {
-        Piece piece = new Piece(PAWN);
         PieceType pieceType = PAWN;
+        Piece piece = new Piece(PAWN);
+        assertEquals(pieceType, piece.getPieceType());
+    }
+    
+    @Test
+    @DisplayName("A knight is a knight")
+    void knightIsKnight() {
+        PieceType pieceType = KNIGHT;
+        Piece piece = new Piece(KNIGHT);
         assertEquals(pieceType, piece.getPieceType());
     }
 
