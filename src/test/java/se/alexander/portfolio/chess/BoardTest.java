d1ab0944cd87137cdb4869e4fd29abed4b450b11
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-09-17 13:51:46 +0200

    Made some intellij suggested refactorings.

--- src/test/java/se/alexander/portfolio/chess/BoardTest.java
+++ src/test/java/se/alexander/portfolio/chess/BoardTest.java
@@ -4,20 +4,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BoardTest {
+class BoardTest {
 
     @Test
     @DisplayName("First index of board array of squares should be square A1")
     void firstIndexShouldBeSquareA1() {
         Square expectedSquare = Square.A1;
         List<Square> squares = new ArrayList<>();
-        for (Square square : Square.values()) {
-            squares.add(square);
-        }
+        Collections.addAll(squares, Square.values());
         Board board = new Board(squares);
         assertEquals(expectedSquare, board.getSquares(0));
     }
